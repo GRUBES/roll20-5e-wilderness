5a643bac1641f823976a93d5d87aec5770f737ed
Eric T Grubaugh <eric@stoic.software>
2018-06-19 21:43:26 -0600

    [WIP] Removed webpack

--- weather/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './src/wild-weather-api.js',
-  output: {
-    filename: 'weather5e.js',
-    path: path.resolve(__dirname, 'dist')
-  }
-};
